a4fdeeb63342f3a5e301b9c7c448e4c693989ef6
talbusaidy <talibalbusaidy1130@gmail.com>
2024-08-27 10:14:49 +0300

    initial commit

---
+++ app/src/main/java/com/example/iblogg/navigation/Routes.kt
@@ -0,0 +1,9 @@
+package com.example.iblogg.navigation
+
+const val ROUTE_SIGNUP = "signup"
+const val ROUTE_LOGIN = "login"
+const val ROUTE_HOME = "Home"
+const val ROUTE_Notification = "Notification"
+const val ROUTE_PROFILE = "Profile"
+const val ROUTE_SEARCH = "Search"
+
---
+++ app/src/main/java/com/example/iblogg/ui/theme/screens/Search.kt
@@ -0,0 +1,97 @@
+package com.example.iblogg.ui.theme.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.iblogg.navigation.ROUTE_HOME
+import com.example.iblogg.navigation.ROUTE_Notification
+import com.example.iblogg.navigation.ROUTE_PROFILE
+import com.example.iblogg.navigation.ROUTE_SEARCH
+import com.example.iblogg.ui.theme.Green
+
+@Composable
+fun Searchscreen(navController: NavController){
+    Scaffold (
+        bottomBar = {
+            BottomAppBar (containerColor = Color.Green,
+                actions = {
+                    IconButton(onClick = { navController.navigate(ROUTE_HOME) }) {
+                        Icon(
+                            Icons.Filled.Home,
+                            contentDescription ="Home Icon" ,
+                            tint = Color.Black)
+                    }
+                    IconButton(onClick = { navController.navigate(ROUTE_Notification) }) {
+                        Icon(
+                            Icons.Filled.Notifications,
+                            contentDescription = "Notification Icon",
+                            tint = Color.Black)
+                    }
+                    IconButton(onClick = {navController.navigate(ROUTE_SEARCH)}) {
+                        Icon(
+                            Icons.Filled.Search,
+                            contentDescription = "Search Icon",
+                            tint = Color.Black)
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { navController.navigate(ROUTE_PROFILE) },
+                        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()) {
+                        Icon(
+                            Icons.Filled.AccountCircle,
+                            contentDescription ="Profile Icon" ,
+                            tint = Color.Black)
+                    }
+                }
+            )
+
+
+
+
+        }
+    ){innerPadding -> Column( modifier = Modifier
+        .padding(innerPadding)
+        .verticalScroll(rememberScrollState())
+        .padding(10.dp)
+        .fillMaxWidth()) {
+
+    }}
+
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SearchscreenPrev(){
+    Searchscreen(rememberNavController())
+}
